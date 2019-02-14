c8229e655dfa30994b73fd40726406f9470fc7a1
Ben Lancaster <bdl@live.co.uk>
2019-02-14 18:54:18 +0000

    dec: Add decoder for latest isa
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

--- vmicro16/vmicro16.v
+++ vmicro16/vmicro16.v
@@ -6,6 +6,8 @@
 /* verilator lint_off BLKSEQ */
 /* verilator lint_off WIDTH */
 
+`include "vmicro16_isa.v"
+
 // This module aims to be a SYNCHRONOUS, WRITE_FIRST BLOCK RAM
 //   https://www.xilinx.com/support/documentation/user_guides/ug473_7Series_Memory_Resources.pdf
 //   https://www.xilinx.com/support/documentation/user_guides/ug383.pdf
@@ -82,3 +84,112 @@ module vmicro16_regs # (
         assign rd1 = regs[rs1];
         assign rd2 = regs[rs2];
 endmodule
+
+
+/* verilator lint_off UNUSED */
+module vmicro16_dec (
+        input         clk,   // not used yet (all combinational)
+        input         reset, // not used yet (all combinational)
+
+        input  [15:0] instr,
+
+        output [4:0]  opcode,
+        output [2:0]  rd,
+        output [2:0]  ra,
+        output [7:0]  imm8,
+        output [11:0] imm12,
+        output [4:0]  simm5,
+
+        output reg [4:0] alu_op,
+
+        output reg   has_imm8,
+        output reg   has_imm12,
+        output reg   is_we,
+        output reg   is_jmp,
+        output reg   is_mem,
+        output reg   is_mem_we,
+        
+        // TODO: Use to identify bad instruction and
+        //       raise exceptions
+        output        is_bad 
+);
+        assign opcode    = instr[15:11];
+        assign rd        = instr[10:8];
+        assign ra        = instr[7:5];
+        assign imm8      = instr[7:0];
+        assign imm12     = instr[11:0];
+        assign simm5     = instr[4:0];
+
+        // alu_op
+        always @(*) case (opcode)
+            `VMICRO16_OP_NOP:               alu_op = `VMICRO16_ALU_NOP;
+        
+            `VMICRO16_OP_LW:                alu_op = `VMICRO16_ALU_LW;
+            `VMICRO16_OP_SW:                alu_op = `VMICRO16_ALU_SW;
+
+            `VMICRO16_OP_MOV:               alu_op = `VMICRO16_ALU_MOV;
+            `VMICRO16_OP_MOVI:              alu_op = `VMICRO16_ALU_MOVI;
+            `VMICRO16_OP_MOVI_L:            alu_op = `VMICRO16_ALU_MOVI_L; 
+             
+            `VMICRO16_OP_BIT: casez (simm5)
+                `VMICRO16_OP_BIT_OR:        alu_op = `VMICRO16_ALU_BIT_OR;
+                `VMICRO16_OP_BIT_XOR:       alu_op = `VMICRO16_ALU_BIT_XOR;
+                `VMICRO16_OP_BIT_AND:       alu_op = `VMICRO16_ALU_BIT_AND;
+                `VMICRO16_OP_BIT_NOT:       alu_op = `VMICRO16_ALU_BIT_NOT;
+                `VMICRO16_OP_BIT_LSHFT:     alu_op = `VMICRO16_ALU_BIT_LSHFT;
+                `VMICRO16_OP_BIT_RSHFT:     alu_op = `VMICRO16_ALU_BIT_RSHFT;
+                default:                    alu_op = `VMICRO16_ALU_BAD; endcase
+
+            `VMICRO16_OP_ARITH_U: casez (simm5)
+                `VMICRO16_OP_ARITH_UADD:    alu_op = `VMICRO16_ALU_ARITH_UADD;
+                `VMICRO16_OP_ARITH_USUB:    alu_op = `VMICRO16_ALU_ARITH_USUB;
+                `VMICRO16_OP_ARITH_UADDI:   alu_op = `VMICRO16_ALU_ARITH_UADDI;
+                default:                    alu_op = `VMICRO16_ALU_BAD; endcase
+            
+            `VMICRO16_OP_ARITH_S: casez (simm5)
+                `VMICRO16_OP_ARITH_SADD:    alu_op = `VMICRO16_ALU_ARITH_SADD;
+                `VMICRO16_OP_ARITH_SSUB:    alu_op = `VMICRO16_ALU_ARITH_SSUB;
+                `VMICRO16_OP_ARITH_SSUBI:   alu_op = `VMICRO16_ALU_ARITH_SSUBI; 
+                default:                    alu_op = `VMICRO16_ALU_BAD; endcase
+        endcase
+
+        always @(*) case (opcode)
+            `VMICRO16_OP_LW,
+            `VMICRO16_OP_MOV,
+            `VMICRO16_OP_MOVI,
+            `VMICRO16_OP_MOVI_L,
+            `VMICRO16_OP_ARITH_U,
+            `VMICRO16_OP_ARITH_S,
+            `VMICRO16_OP_CMP,
+            `VMICRO16_OP_SETC:
+                     is_we = 1'b1;
+            default: is_we = 1'b0;
+        endcase
+
+        always @(*) case (opcode)
+            `VMICRO16_OP_MOVI,
+            `VMICRO16_OP_CMP:
+                     has_imm8 = 1'b1;
+            default: has_imm8 = 1'b0;
+        endcase
+
+        always @(*) case (opcode)
+            `VMICRO16_OP_MOVI_L:
+                     has_imm12 = 1'b1;
+            default: has_imm12 = 1'b0;
+        endcase
+        
+        always @(*) case (opcode)
+            `VMICRO16_OP_BR:
+                     is_jmp = 1'b1;
+            default: is_jmp = 1'b0;
+        endcase
+        
+        always @(*) case (opcode)
+            `VMICRO16_OP_LW,
+            `VMICRO16_OP_SW:
+                     is_mem = 1'b1;
+            default: is_mem = 1'b0;
+        endcase
+endmodule
+

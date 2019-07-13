3261c5d2706d8e190550ee8d26a433584e60dcf9
Ben Lancaster <bdl@live.co.uk>
2019-07-13 22:52:34 +0100

    sw: working sum.s
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

--- sw/demos/asm/sum.s
+++ sw/demos/asm/sum.s
@@ -38,10 +38,10 @@ entry:
     //      ns = 100
     //      nst = ns / (num_threads)
     //      nst = ns >> (num_threads - 1)
-    //      r0 = (num_threads -1)
+    //      r0 = (num_threads -1) WRONG!!!
     mov     r0, r7
     subi    r0, r3 + #0x01
-    movi    r4, #0x64
+    movi    r4, #0x0a
     rshft   r4, r0
     
     // ROOT
@@ -73,19 +73,51 @@ barrier:
     movi    r4, barrier
     br      r4, BR_NE
 
+    // EACH CORE
 synced1:
-    // Now load the nsamples_per_thread
+    // Retrieve load the nsamples_per_thread
     lw      r4, r5 + #0x01
-    // Calculate start index
-    //   nstart = idx * nsamples_per_thread
+    // Calculate nstart = idx * nsamples_per_thread
+    //   in r2
     mov     r2, r6
     mult    r2, r4
 
-    // Loop from: nstart -> (nstart + nsamples_per_thread)
-    movi    r2, #0xcc
-    movi    r2, #0xdd
-    movi    r2, #0xee
-    movi    r2, #0xff
+    // Loop limit in r4
+    // samples_per_thread -> samples_per_thread + nstart
+    add     r4, r2
+
+    // Sum numbers from nstart to limit
+sum_loop:
+    // sum += i
+    add     r1, r2
+    // increment i
+    addi    r2, r3 + #0x01
+    // check end
+    cmp     r2, r4
+    movi    r0, sum_loop
+    br      r0, BR_NE
+
+    // partial sum finished in r1
+    // use mutex to write to shared value
+sum_mutex:
+    // load latest count
+    lwex    r0, r5 + #0x2
+    // try increment count
+    // increment by 1
+    add     r0, r1
+    // make copy as swex has a return value
+    mov     r2, r0
+    // attempt store
+    swex    r0, r5 + #0x02
+    // check success (== 0)
+    cmp     r0, r3
+    // branch if failed
+    movi    r4, sum_mutex
+    br      r4, BR_NE
+
+write_gpio:
+    movi    r3, #0x91
+    sw      r2, r3
 
 exit:
     halt    r0, r0
